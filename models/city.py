4be0f0a43b21d0d53c5ad35186d7dda1a810a76c
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-02-13 06:48:20 -0800

    adding empty class

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" The City class """
+
+from base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Inherits from the BaseModel """
+    pass
---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" The Review class"""
+
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Inherits from the BaseModel """
+    pass
