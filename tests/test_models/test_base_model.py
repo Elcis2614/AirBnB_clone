3e3cb22d922dcdc8011221d0e312dc550cd03c7e
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-02-14 02:19:40 -0800

    first testclass

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+""" This file contains the test class for the base model """
+
+
+import unittest
+
+
+class Base_test(unittest.TestCase):
+    pass
+
+
+if __main__ == "__main__":
+    unittest.main()
