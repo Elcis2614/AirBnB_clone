537dfba0a4a4a755d27df0a66b73e980b0b0ad77
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-02-15 00:05:50 -0800

    test case for the engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+""" This file contains the test class for the engine"""
+
+
+import unittest
+
+
+class testEngine(unittest.TestCases):
+    """ Tests methods """
+    def test_one(self):
+        """ First testing method"""
+        pass
