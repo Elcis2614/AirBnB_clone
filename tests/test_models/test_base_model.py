559ee6a01557add6d59188cd095657a1a9ece4fa
elcis2614 <elisereflorbusole@gmail.com>
2023-05-27 23:27:15 +0000

    wonderful unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,17 +12,22 @@ class Base_test(unittest.TestCase):
         self.base1 = BaseModel()
         self.base2 = BaseModel()
 
+    def tearDown(self):
+        """ Built in method to delete the basemodel object created """
+        del(self.base1)
+        del(self.base2)
+
     def test_save(self):
         """Tests the save method of the baseclass test """
         self.base1.save()
         self.assertNotEqual(self.base1.updated_at, self.base1.created_at)
 
-    def to_dict(self):
+    def test_to_dict(self):
         """ Tests the to_dic method """
-        testDict = self.base1.to_diict()
+        testDict = self.base1.to_dict()
         self.assertIn("__class__", [*testDict])
-        self.assertEqual(testDict['__class__'], self.base1.__classs__.__name__)
-        assertTrue(type(testDict['created_at']) == type(testDict['updated_at']) == str)
-
+        self.assertEqual(testDict['__class__'], self.base1.__class__.__name__)
+        self.assertTrue(type(testDict['created_at']) == type(testDict['updated_at']) == str)
+        
     def test_to_json(self):
         pass
