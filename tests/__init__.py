0b3454dbab79684996509064f0ce370ef500b259
elcis2614 <elisereflorbusole@gmail.com>
2023-05-28 09:27:58 +0000

    documenting everything

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+""" This module contains necessary test cases for all modules and classes of the project 
+Where all files and directories are named like the file of class they test, preceded by test_ prefix and folowing the same organization as the package being tested
+eg : for models/base_model.py , the uni test is : tests/test_models/test_base_model.py
+""""
