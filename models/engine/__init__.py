d61769afdf2a3cdcb5995956b518ff773428c7b5
elcis2614 <elisereflorbusole@gmail.com>
2023-06-05 03:36:02 +0000

    building the engine

---
+++ models/engine/__init__.py
@@ -0,0 +1,4 @@
+""" This module conatins the engine for the project 
+THat is, file fileStorage class to manage serialization of objects
+And, the file.json to store and retrieve sterialised data
+"""
